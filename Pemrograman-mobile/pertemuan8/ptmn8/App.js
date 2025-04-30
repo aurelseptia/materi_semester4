0039aba74e21d842b31eba9303394272ac9c2142
aurelseptia <you@example.com>
2025-04-30 15:43:31 +0700

    pertemuan8

---
+++ Pemrograman-mobile/pertemuan8/ptmn8/App.js
@@ -0,0 +1,12 @@
+import { StatusBar } from 'expo-status-bar';
+import {  Text, View } from 'react-native';
+import tw from "twrnc"
+
+export default function App() {
+  return (
+    <View style={tw`p-4 android:pt-2 bg-white dark:bg-black`}>
+      <Text>Open up App.js to start working on your app!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
