cc0865fd2037b65ac7d8e0fd7d5cd500d7c454cd
aurelseptia <you@example.com>
2025-04-30 15:43:52 +0700

    pt8

--- Pemrograman-mobile/pertemuan8/ptmn8/App.js
+++ Pemrograman-mobile/pertemuan8/ptmn8/App.js
@@ -1,12 +1,29 @@
-import { StatusBar } from 'expo-status-bar';
-import {  Text, View } from 'react-native';
-import tw from "twrnc"
+import {view , text} from 'react native';
+import tw from 'twrnc';
+
+const MyComponent =() => (
+  <View style = {tw`p-4 android:pt-2 bg-white dark:bg-black`}>
+    <Text style = {tw`text-md text-black dark:text-white`}>Hello World</Text>
+    </View>
+);
+import  React, {useState} from 'react';
+import { SafeAreaView,Text, View, Switch  } from 'react-native';
+import  tw from 'twrnc';
 
 export default function App() {
+  const [isDarkMode, setIsDarkMode] = useState(false);
   return (
-    <View style={tw`p-4 android:pt-2 bg-white dark:bg-black`}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    <SafeAreaView style={tw`flex-1 ${isDarkMode ? "bg-gray-900" : "bg-white"}`}>
+      <View style={tw`flex-1 items-center justify-center`}>
+        <Text
+          style={tw`text-3xl font-bold ${
+            isDarkMode ? "text-white" : "text-black"
+          }`}
+        >
+          Hello World
+        </Text>
+        <Switch value={isDarkMode} onValueChange={setIsDarkMode} />
+      </View>
+    </SafeAreaView>
+  );
 }
